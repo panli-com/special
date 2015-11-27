ea27b86d9955c846a519d440c7fcb1ae6d49c949
zanjs <root@zanjs.com>
2015-11-27 09:16:08 +0800

    1212PC up index

--- 20151125/src/js/c_function.js
+++ 20151125/src/js/c_function.js
@@ -1,7 +1,3 @@
-//
-function Log(t){
-  return console.log(t);
-}
 function UserBalance(uName,callback) {  
      $.ajax({
             type: "POST",
@@ -18,6 +14,80 @@ function UserBalance(uName,callback) {
 
             }
         });    
+}
+/*
+** randomWord 产生任意长度随机字母数字组合
+** randomFlag-是否任意长度 min-任意长度最小位[固定位数] max-任意长度最大位
+使用方法
+
+生成3-32位随机串：randomWord(true, 3, 32)
+生成88位随机串：randomWord(false, 88)
+*/
+ 
+function randomWord(randomFlag, min, max){
+    var str = "",
+        range = min,
+        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; 
+    // 随机产生
+    if(randomFlag){
+        range = Math.round(Math.random() * (max-min)) + min;
+    }
+    for(var i=0; i<range; i++){
+        pos = Math.round(Math.random() * (arr.length-1));
+        str += arr[pos];
+    }
+    return str;
+}
+
+//抢代金券领取接口
+function getDouble12(obj,callback) {
+    var radNub = randomWord(false, 18);
+     $.ajax({
+            type: "POST",
+            url: "/App_Services/wsSpecial.asmx/getDouble12?time="+radNub,
+            dataType: "json",
+            data: '' + obj + '',
+            contentType: "application/json;utf-8",
+            timeout: 10000,
+            error: function () {
+                //alert("500");
+            },
+            success: function (data) {
+                callback(data);
+            }
+        });    
+}
+//获取抢代金券剩余数量
+function Coupon_NumberState(callback) {
+    var radNub = randomWord(false, 18);
+     $.ajax({
+            type: "POST",
+            url: "/App_Services/wsSpecial.asmx/Coupon_NumberState?time="+radNub,
+            dataType: "json",
+            data: '',
+            contentType: "application/json;utf-8",
+            timeout: 10000,
+            error: function () {
+                //alert("500");
+            },
+            success: function (data) {
+                callback(data);
+            }
+        });    
+}
+
+
+function ReturnLayer(num){
+  var icon = 5;
+  var num = Number(num);
+  num == 1 ? icon = 6 : "";
+  var text = [
+    "恭喜您成功抢到代金券",
+    "每个人只能领两张券哦",
+    "这张券已经抢过了哦",
+    "啊喔, 这张券已经被大家抢光了呢"
+  ];  
+  PL.alert(text[num-1],{icon: icon});
 }
 
 // v  
--- 20151125/src/js/main.js
+++ 20151125/src/js/main.js
@@ -6,15 +6,17 @@
    //touchend:    
    //touchcancel:     
   
-  $(function(){
+  $(function(){ 
 
       $(".floor-nav-a").on("click",function(){
                 var _t = $(this),
                     _tf = _t.attr("floor");
                 var _afloTop = $("#floor-"+_tf).offset().top;
-                 $('body,html').animate({ scrollTop: _afloTop }, 800);
-    })
-
+                 $('body,html').animate({ scrollTop: _afloTop }, 300);
+      });
+      $("#back-top").on("click",function(){              
+                 $('body,html').animate({ scrollTop: 0 }, 300);
+      })
     
   });
     
